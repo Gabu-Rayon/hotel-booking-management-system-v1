a0e297cd6f74c50a1c3c6192a9ec9812034c256e
Gabu-Rayon <gibsonmurimi4@gmail.com>
2023-11-07 11:28:37 +0300

    first commit

---
+++ aboutus.php
@@ -0,0 +1,220 @@
+<h1>About Us</h1>
+<div class="intro">
+  <div class="container">
+    <div class="row row-eq-height">
+
+      <!-- Intro Content -->
+      <div class="col-lg-6">
+        <div class="intro_content">
+          <div class="section_title">
+            <div>Hello</div>
+            <h1>Amazing Hotel Laong Kenyan Coast Line</h1>
+          </div>
+          <div class="intro_text">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta ipsum natus eaque voluptatem aliquam,
+              quidem quibusdam! A magni corrupti porro doloremque laudantium,
+              corporis nesciunt commodi dicta omnis, error mollitia debitis?</p>
+          </div>
+          <!--  <div class="button intro_button"><a href="#">read more</a></div> -->
+        </div>
+      </div>
+
+      <!-- Intro Image -->
+      <div class="col-lg-6">
+        <div class="intro_image">
+          <div class="background_image" style="background-image:url(images/intro.jpg)"></div>
+          <img src="images/intro.jpg" alt="">
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<!-- Offering -->
+
+<div class="offering">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="section_title text-center">
+          <div>Resort</div>
+          <h1>What we offer</h1>
+        </div>
+      </div>
+    </div>
+    <div class="row offering_row">
+
+      <!-- Offer Item -->
+      <div class="col-xl-4 col-md-6">
+        <div class="offer">
+          <div class="offer_image"><img src="images/offer_1.jpg" alt=""></div>
+          <div class="offer_content text-center">
+            <div class="offer_title">
+              <h3>Outdoor Pool</h3>
+            </div>
+            <div class="offer_text">
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis fugit culpa impedit sequi quae. Debitis, eveniet. 
+                Dolorum reiciendis alias quas modi eaque reprehenderit excepturi recusandae dolore labore, molestias non. Quisquam.
+              </p>
+            </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+          </div>
+        </div>
+      </div>
+
+      <!-- Offer Item -->
+      <div class="col-xl-4 col-md-6">
+        <div class="offer">
+          <div class="offer_image"><img src="images/offer_2.jpg" alt=""></div>
+          <div class="offer_content text-center">
+            <div class="offer_title">
+              <h3>Indoor Pool</h3>
+            </div>
+            <div class="offer_text">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium beatae architecto et illum dolore voluptas
+                 numquam eius voluptate, qui nihil omnis impedit minus ad expedita! Similique
+                 saepe praesentium illo? Consectetur.</p>
+            </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+          </div>
+        </div>
+      </div>
+
+      <!-- Offer Item -->
+      <div class="col-xl-4 col-md-6">
+        <div class="offer">
+          <div class="offer_image"><img src="images/offer_3.jpg" alt=""></div>
+          <div class="offer_content text-center">
+            <div class="offer_title">
+              <h3>Spa Zone</h3>
+            </div>
+            <div class="offer_text">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus natus error molestiae qui magni consequuntur odit earum non, vel,
+                 quibusdam repellat reprehenderit eligendi quod voluptate debitis, sit maiores! Optio, alias.</p>
+            </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+          </div>
+        </div>
+      </div>
+
+      <!-- Offer Item -->
+      <div class="col-xl-4 col-md-6">
+        <div class="offer">
+          <div class="offer_image"><img src="images/offer_4.jpg" alt=""></div>
+          <div class="offer_content text-center">
+            <div class="offer_title">
+              <h3>Sports Area</h3>
+            </div>
+            <div class="offer_text">
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores maxime rem voluptate repellat corporis possimus ut eius, quis nulla et vero doloremque aut deserunt eveniet praesentium 
+                nobis veniam? Asperiores, commodi.</p>
+            </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+          </div>
+        </div>
+      </div>
+
+      <!-- Offer Item -->
+      <div class="col-xl-4 col-md-6">
+        <div class="offer">
+          <div class="offer_image"><img src="images/offer_5.jpg" alt=""></div>
+          <div class="offer_content text-center">
+            <div class="offer_title">
+              <h3>Restaurant</h3>
+            </div>
+            <div class="offer_text">
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti in tempora cum vero mollitia dolor eos voluptatum commodi molestias, 
+                magni libero minus odio corporis. 
+                Iure excepturi aperiam corporis veritatis exercitationem!</p>
+            </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+          </div>
+        </div>
+      </div>
+
+      <!-- Offer Item -->
+      <div class="col-xl-4 col-md-6">
+        <div class="offer">
+          <div class="offer_image"><img src="images/offer_6.jpg" alt=""></div>
+          <div class="offer_content text-center">
+            <div class="offer_title">
+              <h3>Skybar</h3>
+            </div>
+            <div class="offer_text">
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis, aspernatur! Error, sit temporibus sunt, repellendus blanditiis nulla facilis obcaecati, omnis quibusdam alias rerum 
+                ut vitae cumque nobis veritatis tempora similique.</p>
+            </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<!-- Discover -->
+
+<div class="discover">
+
+  <!-- Discover Content -->
+  <div class="discover_content">
+    <div class="container">
+      <div class="row">
+
+        <!-- Discover Content -->
+        <div class="col-xl-5 col-lg-6">
+          <div class="section_title">
+            <div>Hotel</div>
+            <h1>Discover OURS  Hotel</h1>
+          </div>
+          <div class="discover_highlight">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime quibusdam officia, odit deleniti cumque sequi fugiat facere optio 
+              adipisci ab sed! Beatae aspernatur numquam modi architecto voluptatum
+               nihil laudantium laborum!
+              </p>
+          </div>
+          <div class="discover_text">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore accusamus illo non modi voluptatum soluta porro molestiae facere, rem aperiam quisquam, tempore quae 
+              laborum ipsa! Accusantium voluptate nostrum itaque eum!</p>
+          </div>
+          <div class="button discover_button"><a href="#">discover</a></div>
+        </div>
+
+        <!-- Milestones -->
+        <div class="col-lg-6 offset-xl-1">
+          <div class="milestones d-flex flex-row align-items-start justify-content-start flex-wrap">
+
+            <!-- Milestone -->
+            <div class="milestone">
+              <div class="milestone_counter" data-end-value="75">0</div>
+              <div class="milestone_text">Deluxe Rooms</div>
+            </div>
+
+            <!-- Milestone -->
+            <div class="milestone">
+              <div class="milestone_counter" data-end-value="110">0</div>
+              <div class="milestone_text">Years of Experience</div>
+            </div>
+
+            <!-- Milestone -->
+            <div class="milestone">
+              <div class="milestone_counter" data-end-value="31">0</div>
+              <div class="milestone_text">Awards Won</div>
+            </div>
+
+            <!-- Milestone -->
+            <div class="milestone">
+              <div class="milestone_counter" data-end-value="51" data-sign-after="k">0</div>
+              <div class="milestone_text">Happy Clients</div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+</div>
---
+++ home.php
@@ -0,0 +1,353 @@
+<div class="intro">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="section_title text-center">
+          <div>Welcome</div>
+          <h1>Amazing Hotel in Kenyan Coast Line #Mombasa</h1>
+        </div>
+      </div>
+    </div>
+    <div class="row intro_row">
+      <div class="col-xl-8 col-lg-10 offset-xl-2 offset-lg-1">
+        <div class="intro_text text-center">
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis, delectus. Doloribus maxime ad vitae aperiam aut,
+            vel ratione corporis explicabo facilis obcaecati cum molestias ab. Veritatis esse odit consectetur nemo.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse aliquam porro deleniti. Magnam facere nobis,
+            incidunt voluptatum maxime totam nam sequi aliquam quod commodi ipsa assumenda quae, delectus, laudantium quaerat.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="row gallery_row">
+      <div class="col">
+
+        <!-- Gallery -->
+        <div class="gallery_slider_container">
+          <div class="owl-carousel owl-theme gallery_slider">
+
+            <!-- Slide -->
+            <div class="gallery_slide">
+              <img src="images/gallery_1.jpg" alt="">
+              <div class="gallery_overlay">
+                <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                  <a href="#">
+                    <!-- <span>+</span>
+                      <span>See More</span> -->
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <!-- Slide -->
+            <div class="gallery_slide">
+              <img src="images/gallery_2.jpg" alt="">
+              <div class="gallery_overlay">
+                <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                  <a href="#">
+                    <!-- <span>+</span>
+                      <span>See More</span> -->
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <!-- Slide -->
+            <div class="gallery_slide">
+              <img src="images/gallery_3.jpg" alt="">
+              <div class="gallery_overlay">
+                <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                  <a href="#">
+                    <!-- <span>+</span>
+                      <span>See More</span> -->
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <!-- Slide -->
+            <div class="gallery_slide">
+              <img src="images/gallery_4.jpg" alt="">
+              <div class="gallery_overlay">
+                <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                  <a href="#">
+                    <!-- <span>+</span>
+                      <span>See More</span> -->
+                  </a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Rooms -->
+
+<div class="rooms_right container_wrapper">
+  <div class="container">
+    <div class="row row-eq-height">
+
+      <!-- Rooms Image -->
+      <div class="col-xl-6 order-xl-1 order-2">
+        <div class="rooms_slider_container">
+          <div class="owl-carousel owl-theme rooms_slider">
+
+            <!-- Slide -->
+            <div class="slide">
+              <div class="background_image" style="background-image:url(images/rooms_1.jpg)"></div>
+            </div>
+
+            <!-- Slide -->
+            <div class="slide">
+              <div class="background_image" style="background-image:url(images/rooms_1.jpg)"></div>
+            </div>
+
+            <!-- Slide -->
+            <div class="slide">
+              <div class="background_image" style="background-image:url(images/rooms_1.jpg)"></div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <!-- Rooms Content -->
+      <div class="col-xl-6 order-xl-2 order-1">
+        <div class="rooms_right_content">
+          <div class="section_title">
+            <div>Rooms</div>
+            <h1>Luxury Double Suite</h1>
+          </div>
+          <div class="rooms_text">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur praesentium adipisci repudiandae rem atque vel voluptates
+              fugiat quos aperiam ipsa vero sunt saepe provident, dolores, laboriosam obcaecati consequatur quidem numquam.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi exercitationem distinctio quasi saepe optio, doloribus,
+              laboriosam id rerum officiis natus incidunt minus quisquam amet earum nam enim beatae molestias voluptas.</p>
+          </div>
+          <div class="rooms_list">
+            <ul>
+              <li class="d-flex flex-row align-items-center justify-content-start">
+                <img src="images/check.png" alt="">
+                <span>Tempos Ambient</span>
+              </li>
+              <li class="d-flex flex-row align-items-center justify-content-start">
+                <img src="images/check.png" alt="">
+                <span>Tempos Ambient</span>
+              </li>
+              <li class="d-flex flex-row align-items-center justify-content-start">
+                <img src="images/check.png" alt="">
+                <span>Tempos Ambient</span>
+              </li>
+            </ul>
+          </div>
+          <div class="rooms_price">Ksh12900/<span>Night</span></div>
+
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<!-- Rooms -->
+
+<div class="rooms_left container_wrapper">
+  <div class="container">
+    <div class="row row-eq-height">
+
+      <!-- Rooms Content -->
+      <div class="col-xl-6">
+        <div class="rooms_left_content">
+          <div class="section_title">
+            <div>Rooms</div>
+            <h1>Luxury Single Room</h1>
+          </div>
+          <div class="rooms_text">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, temporibus
+              sit! Ipsam sequi autem cupiditate eius dolores amet odit. Quo porro dolorum assumenda incidunt ea natus in, ut cum placeat!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem nesciunt et optio modi itaque enim,
+              facilis beatae maxime aliquid ea officiis distinctio laborum illum architecto totam! Odio in repellat officia.
+            </p>
+          </div>
+          <div class="rooms_list">
+            <ul>
+              <li class="d-flex flex-row align-items-center justify-content-start">
+                <img src="images/check.png" alt="">
+                <span>Tempos Ambient</span>
+              </li>
+              <li class="d-flex flex-row align-items-center justify-content-start">
+                <img src="images/check.png" alt="">
+                <span>Tempos Ambient</span>
+              </li>
+              <li class="d-flex flex-row align-items-center justify-content-start">
+                <img src="images/check.png" alt="">
+                <span>Wells of Sea</span>
+              </li>
+            </ul>
+          </div>
+          <div class="rooms_price">Ksh18900/<span>Night</span></div>
+
+        </div>
+      </div>
+
+      <!-- Rooms Image -->
+      <div class="col-xl-6">
+        <div class="rooms_slider_container">
+          <div class="owl-carousel owl-theme rooms_slider">
+
+            <!-- Slide -->
+            <div class="slide">
+              <div class="background_image" style="background-image:url(images/rooms_2.jpg)"></div>
+            </div>
+
+            <!-- Slide -->
+            <div class="slide">
+              <div class="background_image" style="background-image:url(images/rooms_2.jpg)"></div>
+            </div>
+
+            <!-- Slide -->
+            <div class="slide">
+              <div class="background_image" style="background-image:url(images/rooms_2.jpg)"></div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<!-- Discover -->
+
+<div class="discover">
+
+  <!-- Discover Content -->
+  <div class="discover_content">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5">
+          <div class="section_title">
+            <div>Hotel</div>
+            <h1>Discover OURS Hotel</h1>
+          </div>
+        </div>
+      </div>
+      <div class="row discover_row">
+        <div class="col-lg-5">
+          <div class="discover_highlight">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, laudantium ex assumenda quae
+              fuga cumque accusantium facilis tenetur odit consequuntur obcaecati? Dicta, tempora. Odit, ipsum quas aliquam est delectus officia.</p>
+          </div>
+          <!-- <div class="button discover_button"><a href="#">discover</a></div> -->
+        </div>
+        <div class="col-lg-7">
+          <div class="discover_text">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum minus accusantium eaque. Cumque reprehenderit tenetur porro fugiat
+              dolore a, quis ab eligendi dolor similique id consequuntur, illo, vero consectetur sapiente?</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Discover Slider -->
+  <div class="discover_slider_container">
+    <div class="owl-carousel owl-theme discover_slider">
+
+      <!-- Slide -->
+      <div class="slide">
+        <div class="background_image" style="background-image:url(images/discover_1.jpg)"></div>
+        <div class="discover_overlay d-flex flex-column align-items-center justify-content-center">
+          <h1><a href="#">Weddings</a></h1>
+        </div>
+      </div>
+
+      <!-- Slide -->
+      <div class="slide">
+        <div class="background_image" style="background-image:url(images/discover_2.jpg)"></div>
+        <div class="discover_overlay d-flex flex-column align-items-center justify-content-center">
+          <h1><a href="#">Parties</a></h1>
+        </div>
+      </div>
+
+      <!-- Slide -->
+      <div class="slide">
+        <div class="background_image" style="background-image:url(images/discover_3.jpg)"></div>
+        <div class="discover_overlay d-flex flex-column align-items-center justify-content-center">
+          <h1><a href="#">Relax</a></h1>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+</div>
+
+<!-- Testimonials -->
+
+<div class="testimonials">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="section_title text-center">
+          <div>Clients</div>
+          <h1>Testimonials</h1>
+        </div>
+      </div>
+    </div>
+    <div class="row testimonials_row">
+      <div class="col">
+
+        <!-- Testimonials Slider -->
+        <div class="testimonials_slider_container">
+          <div class="owl-carousel owl-theme testimonials_slider">
+
+            <!-- Slide -->
+            <div>
+              <div class="testimonial_text text-center">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, maxime ut tempora consequuntur error alias deserunt obcaecati,
+                  laboriosam culpa fugiat officiis enim natus! Provident tenetur possimus debitis, iusto quaerat blanditiis.</p>
+              </div>
+              <div class="testimonial_author text-center">
+                <div class="testimonial_author_image"><img src="images/author_1.jpg" alt=""></div>
+                <div class="testimonial_author_name"><a href="#">Maria Smith,</a><span> Client</span></div>
+              </div>
+            </div>
+
+            <!-- Slide -->
+            <div>
+              <div class="testimonial_text text-center">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum dignissimos alias qui nostrum dolores, esse maxime culpa similique
+                  atque quos dolor! Aliquam ad ullam impedit hic voluptates obcaecati, quidem laborum.</p>
+              </div>
+              <div class="testimonial_author text-center">
+                <div class="testimonial_author_image"><img src="images/author_1.jpg" alt=""></div>
+                <div class="testimonial_author_name"><a href="#">Maria Smith,</a><span> Client</span></div>
+              </div>
+            </div>
+
+            <!-- Slide -->
+            <div>
+              <div class="testimonial_text text-center">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. In modi voluptate temporibus quidem consequuntur ipsa porro distinctio ullam nisi cumque
+                  iure quia tempore laboriosam nobis,
+                  obcaecati deleniti laudantium possimus facilis.</p>
+              </div>
+              <div class="testimonial_author text-center">
+                <div class="testimonial_author_image"><img src="images/author_1.jpg" alt=""></div>
+                <div class="testimonial_author_name"><a href="#">Maria Smith,</a><span> Client</span></div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
